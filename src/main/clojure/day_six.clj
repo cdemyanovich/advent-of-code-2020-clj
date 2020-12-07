5d29a7bc8366c8aa9ae8e24d71b5533f357e87cd
Craig Demyanovich <cdemyanovich@gmail.com>
2020-12-07 00:50:42 -0600

    Day 6, part 2

--- src/main/clojure/day_six.clj
+++ src/main/clojure/day_six.clj
@@ -1,5 +1,6 @@
 (ns day-six
   (:require [clojure.java.io :as io]
+            [clojure.set :as set]
             [clojure.string :as str]))
 
 (def input (slurp (io/resource "data/6-1.txt")))
@@ -11,5 +12,14 @@
        (map count)
        (reduce +)))
 
+(defn part-two [answers]
+  (->> (str/split answers #"\n\n")
+       (map str/split-lines)
+       (map #(map set %))
+       (map #(apply set/intersection %))
+       (map count)
+       (reduce +)))
+
 (comment
-  (= 7128 (part-one input)))
+  (= 7128 (part-one input))
+  (= 3640 (part-two input)))
