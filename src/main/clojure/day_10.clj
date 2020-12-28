a5b92111575553291584816061a2682a3ada36ac
Craig Demyanovich <cdemyanovich@gmail.com>
2020-12-27 20:59:36 -0600

    Day 10, part 2

--- src/main/clojure/day_10.clj
+++ src/main/clojure/day_10.clj
@@ -83,6 +83,18 @@
        (select-vals [1 3])
        (apply *)))
 
+; inspired by https://github.com/JoshCheek/advent_of_code/tree/64848a82e30fdc8d614c2ebefd12407e1df7d3cd/2020/10
+(defn part-two
+  [joltages]
+  (loop [counts {0 1}
+         joltages (sort joltages)]
+    (if (= 0 (count joltages))
+      (last (vals (sort counts)))
+      (recur (assoc counts (first joltages) (+ (get counts (- (first joltages) 1) 0)
+                                               (get counts (- (first joltages) 2) 0)
+                                               (get counts (- (first joltages) 3) 0)))
+             (rest joltages)))))
+
 (defn read-joltages
   []
   (->> "data/10-1.txt"
@@ -95,4 +107,11 @@
   (= 35 (part-one sample-joltages-1-1))
   (= 220 (part-one sample-joltages-1-2))
   (= 2240 (part-one (read-joltages)))
+  (= 99214346656768 (part-two (read-joltages)))
   ,)
+
+; Other interesting approaches:
+; * https://github.com/ocisly/advent2020/blob/a2de1307c5d5527702d2d426b10735a3c69c0e51/day-10.clj
+; * https://cestlaz.github.io/post/advent-2020-day0910/
+; * https://www.reddit.com/r/adventofcode/comments/kh2nr8/2020_day_10_part_2_a_closedform_mathematical/
+; * https://www.reddit.com/r/adventofcode/comments/kabi91/2020_day_10_closedform_mathematical_solution/
